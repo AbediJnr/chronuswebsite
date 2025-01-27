b2257bcb060080b584830a67d3a78db4cbeac9d6
larrybrazz <99844420+larrybrazz@users.noreply.github.com>
2025-01-27 19:18:14 +0000

    First Repo

---
+++ projects.php
@@ -0,0 +1,300 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+    <head>
+        <!-- Meta Tags -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="keywords" content="Chronus, Project, Management, Charity">
+		<meta name="description" content="">
+		<meta name='copyright' content='Chronus Solutions'>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+		<!-- Title -->
+        <title>Chronus Solutions - Your Projects' Professionals</title>
+		
+		<!-- Favicon -->
+		<link rel="icon" type="image/x-icon" href="favicon.ico">
+		
+		<!-- Google Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<!-- Nice Select CSS -->
+		<link rel="stylesheet" href="css/nice-select.css">
+		<!-- Font Awesome CSS -->
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+		<!-- icofont CSS -->
+        <link rel="stylesheet" href="css/icofont.css">
+		<!-- Slicknav -->
+		<link rel="stylesheet" href="css/slicknav.min.css">
+		<!-- Owl Carousel CSS -->
+        <link rel="stylesheet" href="css/owl-carousel.css">
+		<!-- Datepicker CSS -->
+		<link rel="stylesheet" href="css/datepicker.css">
+		<!-- Animate CSS -->
+        <link rel="stylesheet" href="css/animate.min.css">
+		<!-- Magnific Popup CSS -->
+        <link rel="stylesheet" href="css/magnific-popup.css">
+		
+		<!-- CSS -->
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="css/responsive.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<link rel="stylesheet" href="form.css">
+        <script src="form.js"></script>
+		
+    </head>
+    
+    <body class="is-preload">
+	
+		<!-- Preloader -->
+        <div class="preloader">
+            <div class="loader">
+                <div class="loader-outter"></div>
+                <div class="loader-inner"></div>
+
+                <div class="indicator"> 
+                    <svg width="24px" height="24px">
+                        <polyline id="back" points="12,2 8,8 2,12 8,16 12,22 15,16 22,12 15,7 12,2"></polyline>
+                        <polyline id="front" points="12,2 8,8 2,12 8,16 12,22 15,16 22,12 15,7 12,2"></polyline>
+                    </svg>
+                </div>
+            </div>
+        </div>
+
+		<!-- Header Area -->
+		<header class="header" >
+			<!-- Topbar -->
+			<div class="topbar">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-6 col-md-5 col-12">
+													
+							<ul class="top-link">
+									<li><a href="https://www.facebook.com/profile.php?id=61553116751154" target="_blank"><i class="icofont-facebook"></i></a></li>
+									<!--li><a href="#"><i class="icofont-google-plus"></i></a></li-->
+									<li><a href="https://x.com/ChronusSol?t=Odip_3eYnZaapua06bP4JA&s=09" target="_blank"><i class="icofont-twitter"></i></a></li>
+									<li><a href="https://www.instagram.com/chronussolutions?igsh=MWlnbW1jbnliMXZldw==" target="_blank"><i class="icofont-instagram"></i></a></li>
+									<!--li><a href="#"><i class="icofont-pinterest"></i></a></li-->
+								</ul>
+							
+						</div>
+						<div class="col-lg-6 col-md-7 col-12">
+							<!-- Top Contact -->
+							<ul class="top-contact">
+								<!--<li><i class="fa fa-phone"></i>+44 70000 00000</li>-->
+								<li><i class="fa fa-envelope"></i><a href="mailto:info@chronussolutions.co.uk">info@chronussolutions.co.uk</a></li>
+							</ul>
+							<!-- End Top Contact -->
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- End Topbar -->
+			<!-- Header Inner -->
+			<div class="header-inner">
+				<div class="container">
+					<div class="inner">
+						<div class="row">
+							<div class="col-lg-3 col-md-3 col-12">
+								<!-- Start Logo -->
+								<div class="logo">
+									<a href="index.php"><img src="img/logo.png" alt="#"></a>
+								</div>
+								<!-- End Logo -->
+								<!-- Mobile Nav -->
+								<div class="mobile-nav"></div>
+								<!-- End Mobile Nav -->
+							</div>
+							<div class="col-lg-7 col-md-9 col-12">
+								<!-- Main Menu -->
+								<div class="main-menu">
+									<nav class="navigation">
+										<ul class="nav menu">
+											<li><a href="index.php">Home</a>
+												<!--ul class="dropdown">
+													<li><a href="#">Welcome</a></li>
+												</ul-->
+											</li>
+											<li><a href="about.php">About Us</a></li>
+											<li><a href="services.php">Our Services </a></li>
+											<li class="active"><a href="projects.php">Projects</a></li>
+											<li><a href="trainings.php">Trainings</a></li>
+											<li><a href="contact/">Contact Us</a></li>
+										</ul>
+									</nav>
+								</div>
+								<!--/ End Main Menu -->
+							</div>
+							<div class="col-lg-2 col-12">
+								<div class="get-quote">
+									<a href="contact/" class="btn">Get Involved</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--/ End Header Inner -->
+		</header>
+		<!-- End Header Area -->
+
+
+		<!-- Breadcrumbs -->
+		<div class="breadcrumbs overlay">
+			<div class="container">
+				<div class="bread-inner">
+					<div class="row">
+						<div class="col-12">
+							<h2>Projects at Chronus</h2>
+							<ul class="bread-list">
+								<li><a href="index.html">Home</a></li>
+								<li><i class="icofont-simple-right"></i></li>
+								<li class="active">Projects</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- End Breadcrumbs -->
+	
+		<!-- Start Projects Details Area -->
+		
+		 <?php
+$projects = [
+    [
+        "title" => "Chronus Website Project 1.0",
+        "status" => "completed",
+        "date" => "03 March 2024",
+        "overview" => "Designed a modern, user-friendly website to enhance Chronus Solutions’ online presence and improve accessibility for clients."
+    ],
+    [
+        "title" => "Project Uche",
+        "status" => "completed",
+        "date" => "05 May 2024",
+        "overview" => "Developed a scalable, multivendor e-commerce platform, enabling seamless interactions between vendors and customers in a user-friendly interface."
+    ],
+    [
+        "title" => "Project Uddy",
+        "status" => "completed",
+        "date" => "01 June 2024",
+        "overview" => "Created a personalized website for a music artist, providing fans with an easy way to connect and stream music."
+    ],
+    [
+        "title" => "Project E-Sekure",
+        "status" => "in-progress",
+        "progress"=> 70,
+        "date" => "06 July 2024",
+        "overview" => "Developing a secure, modern platform aimed at providing innovative safety solutions for individuals and organizations."
+    ],
+    [
+        "title" => "Chronus Website Project 2.0",
+        "progress"=> 60,
+        "status" => "in-progress",
+        "date" => "03 October 2024",
+        "overview" => "A complete revamp of Chronus 1.0, introducing enhanced features, improved design, and a more intuitive user experience."
+    ],
+    [
+        "title" => "Project AnkaraAfrica",
+        "status" => "in-progress",
+        "progress"=> 80,
+        "date" => "03 November 2024",
+        "overview" => "Creating a robust multivendor e-commerce platform to connect African sellers and buyers in a dynamic marketplace."
+    ],
+    [
+        "title" => "Project Sure Care",
+        "status" => "in-progress",
+        "progress"=> 30,
+        "date" => "03 December 2024",
+        "overview" => "Consultancy project aimed at improving operational efficiency and service delivery for a domiciliary care service provider."
+    ],
+
+    [
+        "title" => "Project Green Planet",
+        "status" => "in-progress",
+        "progress"=> 50,
+        "date" => "13 April 2024",
+        "overview" => "Developing an initiative to promote sustainability and environmental awareness through innovative solutions and community engagement."
+    ]
+];
+
+        ?>
+
+        <section class="news-single section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-12">
+                        <?php foreach ($projects as $project): ?>
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="single-main">
+                                        <!-- Project Title -->
+                                        <h1 class="news-title">
+                                            <a href="#"><?= $project['title']; ?></a>
+                                        </h1>
+                                        <!-- Meta Information -->
+                                       <div class="meta">
+    <div class="meta-left">
+        <span class="date">
+            <i class="fa fa-clock-o"></i> Commenced: <?= $project['date']; ?>
+        </span>
+    </div>
+    <div class="meta-right" style="margin-top: 10px;">
+        <h6>Status:</h6>
+        <?php if ($project['status'] == 'completed'): ?>
+            <i class="fa fa-check-circle fa-2x" style="color: green;"></i> Completed
+        <?php else: ?>
+            <div class="progress" style="height: 20px;">
+                <div class="progress-bar progress-bar-striped progress-bar-animated" 
+                     role="progressbar" 
+                     style="width: <?= $project['progress']; ?>%;" 
+                     aria-valuenow="<?= $project['progress']; ?>" 
+                     aria-valuemin="0" 
+                     aria-valuemax="100">
+                     <?= $project['progress']; ?>% Complete
+                </div>
+            </div>
+        <?php endif; ?>
+    </div>
+</div>
+                                        <!-- Project Overview -->
+                                        <div class="news-text">
+                                            <h6>Project Overview:</h6>
+                                            <p><?= $project['overview']; ?></p>
+                                        </div>
+                                        <!-- Conditional Join Button -->
+                                        <?php if ($project['status'] != 'completed'): ?>
+                                            <a href="contact/" class="btn">Join the Project</a>
+                                        <?php endif; ?>
+                                    </div>
+                                </div>
+                            </div>
+                            <br/>
+                        <?php endforeach; ?>
+                    </div>
+                    <div class="col-lg-4 col-12">
+                        <!-- Sidebar -->
+                        <div class="main-sidebar">
+                            <!-- Categories Widget -->
+                            <div class="single-widget category">
+                                <h3 class="title">Project Categories</h3>
+                                <ul class="categor-list">
+                                    <li><a href="#">Non-profit and Charities</a></li>
+                                    <li><a href="#">Environmental Sustainability</a></li>
+                                    <li><a href="#">Website Development</a></li>
+                                    <li><a href="#">Training and Education</a></li>
+                                    <li><a href="#">Sales and Marketing</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+		
+		<!-- End Projects Details Area -->
+		
+	<?php include 'ft.html'; ?>
---
+++ services.php
@@ -0,0 +1,233 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+    <head>
+        <!-- Meta Tags -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="keywords" content="Chronus, Project, Management, Charity">
+		<meta name="description" content="">
+		<meta name='copyright' content='Chronus Solutions'>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+		<!-- Title -->
+        <title>Chronus Solutions - Your Projects' Professionals</title>
+		
+		<!-- Favicon -->
+		<link rel="icon" type="image/x-icon" href="favicon.ico">
+		
+		<!-- Google Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<!-- Nice Select CSS -->
+		<link rel="stylesheet" href="css/nice-select.css">
+		<!-- Font Awesome CSS -->
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+		<!-- icofont CSS -->
+        <link rel="stylesheet" href="css/icofont.css">
+		<!-- Slicknav -->
+		<link rel="stylesheet" href="css/slicknav.min.css">
+		<!-- Owl Carousel CSS -->
+        <link rel="stylesheet" href="css/owl-carousel.css">
+		<!-- Datepicker CSS -->
+		<link rel="stylesheet" href="css/datepicker.css">
+		<!-- Animate CSS -->
+        <link rel="stylesheet" href="css/animate.min.css">
+		<!-- Magnific Popup CSS -->
+        <link rel="stylesheet" href="css/magnific-popup.css">
+		
+		<!-- CSS -->
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="css/responsive.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<link rel="stylesheet" href="form.css">
+        <script src="form.js"></script>
+		
+    </head>
+    
+    <body class="is-preload">
+	
+		<!-- Preloader -->
+        <div class="preloader">
+            <div class="loader">
+                <div class="loader-outter"></div>
+                <div class="loader-inner"></div>
+
+                <div class="indicator"> 
+                    <svg width="24px" height="24px">
+                        <polyline id="back" points="12,2 8,8 2,12 8,16 12,22 15,16 22,12 15,7 12,2"></polyline>
+                        <polyline id="front" points="12,2 8,8 2,12 8,16 12,22 15,16 22,12 15,7 12,2"></polyline>
+                    </svg>
+                </div>
+            </div>
+        </div>
+        <!-- End Preloader -->
+		
+		<!-- Get Pro Button >
+		<ul class="pro-features">
+			<a class="get-pro" href="#">Let's Chat</a>
+			<!--li class="big-title">Pro Version Available on Themeforest</li>
+			<li class="title">A team member will join you shortly</li>
+			<li>All our representatives are currently engaged, a chat box will appear here once someone is available.</li>
+			<li>Thank you for waiting</li>
+			<!--li>Color Plate With 12+ Colors</li>
+			<li>Sticky Header / Sticky Filters</li>
+			<li>Working Contact Form With Google Map</li>
+			<div class="button">
+				<a href="contact/" target="_blank" class="btn">Join the Queue</a>
+				<a href="contact/" target="_blank" class="btn">Request a Callback</a>
+			</div>
+		</ul>
+	
+		<!-- Header Area -->
+		<header class="header" >
+			<!-- Topbar -->
+			<div class="topbar">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-6 col-md-5 col-12">
+							<!-- Contact >
+							<ul class="top-link">
+								<li><a href="#">About</a></li>
+								<li><a href="#">Projects</a></li>
+								<li><a href="#">Contact</a></li>
+								<li><a href="#">FAQ</a></li>
+							</ul>
+							<!-- End Contact -->
+							
+							<ul class="top-link">
+									<li><a href="https://www.facebook.com/profile.php?id=61553116751154" target="_blank"><i class="icofont-facebook"></i></a></li>
+									<!--li><a href="#"><i class="icofont-google-plus"></i></a></li-->
+									<li><a href="https://x.com/ChronusSol?t=Odip_3eYnZaapua06bP4JA&s=09" target="_blank"><i class="icofont-twitter"></i></a></li>
+									<li><a href="https://www.instagram.com/chronussolutions?igsh=MWlnbW1jbnliMXZldw==" target="_blank"><i class="icofont-instagram"></i></a></li>
+									<!--li><a href="#"><i class="icofont-pinterest"></i></a></li-->
+								</ul>
+							
+						</div>
+						<div class="col-lg-6 col-md-7 col-12">
+							<!-- Top Contact -->
+							<ul class="top-contact">
+								<!--<li><i class="fa fa-phone"></i>+44 70000 00000</li>-->
+								<li><i class="fa fa-envelope"></i><a href="mailto:info@chronussolutions.co.uk">info@chronussolutions.co.uk</a></li>
+							</ul>
+							<!-- End Top Contact -->
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- End Topbar -->
+			<!-- Header Inner -->
+			<div class="header-inner">
+				<div class="container">
+					<div class="inner">
+						<div class="row">
+							<div class="col-lg-3 col-md-3 col-12">
+								<!-- Start Logo -->
+								<div class="logo">
+									<a href="index.php"><img src="img/logo.png" alt="#"></a>
+								</div>
+								<!-- End Logo -->
+								<!-- Mobile Nav -->
+								<div class="mobile-nav"></div>
+								<!-- End Mobile Nav -->
+							</div>
+							<div class="col-lg-7 col-md-9 col-12">
+								<!-- Main Menu -->
+								<div class="main-menu">
+									<nav class="navigation">
+										<ul class="nav menu">
+											<li><a href="index.php">Home</a>
+												<!--ul class="dropdown">
+													<li><a href="#">Welcome</a></li>
+												</ul-->
+											</li>
+											<li><a href="about.php">About Us</a></li>
+											<li class="active"><a href="services.php">Our Services </a></li>
+											<li><a href="projects.php">Projects</a></li>
+											<li><a href="trainings.php">Trainings</a></li>
+											<li><a href="contact/">Contact Us</a></li>
+										</ul>
+									</nav>
+								</div>
+								<!--/ End Main Menu -->
+							</div>
+							<div class="col-lg-2 col-12">
+								<div class="get-quote">
+									<a href="contact/" class="btn">Get Involved</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--/ End Header Inner -->
+		</header>
+		<!-- End Header Area -->
+
+
+		<!-- Breadcrumbs -->
+		<div class="breadcrumbs overlay">
+			<div class="container">
+				<div class="bread-inner">
+					<div class="row">
+						<div class="col-12">
+							<h2>WHAT WE DO</h2>
+							<ul class="bread-list">
+								<li><a href="index.php">Home</a></li>
+								<li><i class="icofont-simple-right"></i></li>
+								<li class="active">Our Services</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- End Breadcrumbs -->
+	
+		<!-- Start Portfolio Details Area -->
+		<section class="news-single section">
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						
+							
+							
+							<div class="body-text">
+								<p><h3>At Chronus Solutions,</h3>
+								<p>We offer a comprehensive range of services designed to meet all your IT needs. From website development to cybersecurity solutions, we have the expertise and experience to help your business thrive in today's digital landscape. </p> <br/>
+								
+								<p><h5>Managed IT Services</h5>
+								Our Managed IT Services are designed to keep your systems running smoothly and efficiently. From troubleshooting and maintenance to proactive monitoring and support, we provide all the essential services you need to ensure your IT infrastructure is always up and running.</p><br/>
+								
+								<p><h5>Trainings</h5>
+								Empower your team with our customized training programs tailored to your specific needs. Whether you're looking to learn new software or improve your IT skills, our expert trainers will help you achieve your goals and stay ahead of the curve.</p><br/>
+								
+								<p><h5>Cybersecurity Solutions</h5>
+								Protect your valuable assets with our comprehensive cybersecurity solutions. From antivirus software to firewall protection, we have everything you need to keep your data safe and secure from cyber threats.</p><br/>
+								
+								<p><h5>Cloud Computing Solutions</h5>
+								Unlock new possibilities for your business with our cloud computing solutions. Whether you're looking to migrate to the cloud or optimize your existing cloud infrastructure, we can help you leverage the power of the cloud to drive innovation and growth.</p><br/>
+								
+								<p><h5>Website Development</h5>
+								Craft a compelling online presence with our expert website development services. From design and development to optimization and maintenance, we'll work with you every step of the way to create a website that reflects your brand and engages your audience.</p><br/>
+								
+								<p><h5>Software Development</h5>
+								Bring your ideas to life with our custom software development solutions. Whether you need a mobile app, a web application, or a desktop software solution, our team of experienced developers will turn your vision into reality, on time and within budget.</p><br/>
+								
+								<p><h5>Strategic Initiatives</h5>
+								At Chronus Solutions, we're committed to giving back to the community. That's why we offer free IT services for non-profits and charitable organizations. This initiative serves as both a portfolio and a testing ground for our capabilities. By supporting these organizations, we generate exposure, referrals, and leads, all while making a positive difference in the world.</p><br/>
+								
+								<p><h5>Business Analysis/Consulting</h5>
+								we specialize in business analysis and consulting services tailored to empower your organization. Our dedicated team of experts combines industry experience with innovative strategies to drive efficiency, optimize processes, and accelerate growth.</p><br/>
+								
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Portfolio Details Area -->
+		
+	<?php include 'ft.html'; ?>
